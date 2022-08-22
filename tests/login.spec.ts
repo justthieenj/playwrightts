b6e14cd9b00eef1aea47f29de46f544a57d8d3e7
Thien Nguyen <ngkhanhthien94@gmail.com>
2022-08-23 00:38:01 +0700

    :zap: implement login with totp

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,9 +1,13 @@
 import { test, expect } from "@playwright/test";
 import LoginPage from "../src/pages/LoginPage";
+import IdentityPage from "../src/pages/IdentityPage";
+import * as account from "./../account.json";
 
 test("Test login sts insider", async ({ page }) => {
   await page.goto("https://identity.saigontechnology.vn/");
 
   const loginPage = new LoginPage(page);
-  await loginPage.login("empcode", "password");
+  await loginPage.login(account.empCode, account.password);
+  const identityPage = new IdentityPage(page);
+  await expect(identityPage.labelWelcome).toBeVisible();
 });
