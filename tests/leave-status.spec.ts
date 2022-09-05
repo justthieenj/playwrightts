7b33467c5c25f2ba791ef3f088664c045b24e4d1
Thien Nguyen <ngkhanhthien94@gmail.com>
2022-09-05 14:50:12 +0700

    :syringe: apply test.step

--- tests/leave-status.spec.ts
+++ tests/leave-status.spec.ts
@@ -1,21 +1,33 @@
-import { test, expect } from "@playwright/test";
-import LoginPage from "../src/pages/identity/LoginPage";
-import IdentityPage from "../src/pages/identity/IdentityPage";
-import * as account from "../account.json";
-import { identityURL, leaveURL } from "../src/utils/constants";
-import DashBoardPage from "../src/pages/leave/DashboardPage";
+import { expect, test } from "@playwright/test";
 import * as fs from "fs";
+import * as account from "../account.json";
+import IdentityPage from "../src/pages/identity/IdentityPage";
+import LoginPage from "../src/pages/identity/LoginPage";
+import DashBoardPage from "../src/pages/leave/DashboardPage";
+import { identityURL, leaveURL } from "../src/utils/constants";
 
 test("Get Leave today list", async ({ page }) => {
-  await page.goto(identityURL);
+  await test.step(`Navigate to ${identityURL}`, async () => {
+    await page.goto(identityURL);
+  });
 
-  const loginPage = new LoginPage(page);
-  await loginPage.login(account.empCode, account.password);
-  const identityPage = new IdentityPage(page);
-  await expect(identityPage.labelWelcome).toBeVisible();
-  await page.goto(leaveURL);
-  const leavePage = new DashBoardPage(page);
-  const a = await leavePage.getLeaveTodayList();
+  await test.step("Login", async () => {
+    const loginPage = new LoginPage(page);
+    await loginPage.login(account.empCode, account.password);
+    const identityPage = new IdentityPage(page);
+    await expect(identityPage.labelWelcome).toBeVisible();
+  });
+
+  await test.step(`Navigate to ${leaveURL}`, async () => {
+    await page.goto(leaveURL);
+  });
+
+  let empList;
+  await test.step("Get today off employees", async () => {
+    const leavePage = new DashBoardPage(page);
+    empList = await leavePage.getLeaveTodayList();
+  });
+
   // save to file
-  fs.writeFileSync("emp_off_today.txt", a.toString());
+  fs.writeFileSync("emp_off_today.txt", empList.toString());
 });
--- tests/logtime.spec.ts
+++ tests/logtime.spec.ts
@@ -6,17 +6,24 @@ import InsiderPage from "../src/pages/insider/InsiderPage";
 import { identityURL, insiderURL } from "../src/utils/constants";
 
 test("Test log time insider with dropdown custom locator", async ({ page }) => {
-  await page.goto(identityURL);
-  const loginPage = new LoginPage(page);
-  await loginPage.login(account.empCode, account.password);
-  const identityPage = new IdentityPage(page);
-  await expect(identityPage.labelWelcome).toBeVisible();
+  await test.step(`Navigate to ${identityURL}`, async () => {
+    await page.goto(identityURL);
+  });
 
-  //redirect to Insider Page
-  await page.goto(insiderURL);
+  await test.step("Login", async () => {
+    const loginPage = new LoginPage(page);
+    await loginPage.login(account.empCode, account.password);
+    const identityPage = new IdentityPage(page);
+    await expect(identityPage.labelWelcome).toBeVisible();
+  });
 
-  // log time popup
-  const insiderPage = new InsiderPage(page);
-  await insiderPage.openTodayLogTimeModal();
-  await insiderPage.logTime("8", "1x - Normal working days", "Code", "Project X", "Check input comment with 20 characters");
+  await test.step(`Navigate to ${insiderURL}`, async () => {
+    await page.goto(insiderURL);
+  });
+
+  await test.step("Log time today", async () => {
+    const insiderPage = new InsiderPage(page);
+    await insiderPage.openTodayLogTimeModal();
+    await insiderPage.logTime("8", "1x - Normal working days", "Code", "Project X", "Check input comment with 20 characters");
+  });
 });
