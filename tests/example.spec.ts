d184691ed15aea2c8691030e2fea1e7fa73a4da7
Thien Nguyen <ngkhanhthien94@gmail.com>
2022-09-06 10:44:05 +0700

    :rocket: create manual trigger config

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('homepage has Playwright in title and get started link linking to the intro page', async ({ page }) => {
+test('Test playwright page', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
