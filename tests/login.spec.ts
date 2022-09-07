20c20a72358b08082486b69120115d9f77b37004
Thien Nguyen <ngkhanhthien94@gmail.com>
2022-09-07 11:09:09 +0700

    :rocket: deploy to website after a manual trigger

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,12 +2,14 @@ import { expect, test } from "@playwright/test";
 import IdentityPage from "../src/pages/identity/IdentityPage";
 import LoginPage from "../src/pages/identity/LoginPage";
 import { accountData as account } from "../src/utils/data-reader";
+import { sendResultNoti } from "../src/utils/slack";
 
-test("Test login sts insider", async ({ page }) => {
+test("Verify user can login with valid credentials", async ({ page, request }, testResult) => {
   await page.goto("https://identity.saigontechnology.vn/");
 
   const loginPage = new LoginPage(page);
   await loginPage.login(account.empCode, account.password);
   const identityPage = new IdentityPage(page);
   await expect(identityPage.labelWelcome).toBeVisible();
+  await sendResultNoti(request, testResult);
 });
