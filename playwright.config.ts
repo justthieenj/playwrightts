cbc4fc34261e04dda4cec3f4dbf39079d29cb045
ngoc.vo <ngoc.vo@saigontechnology.com>
2022-09-08 21:22:12 +0700

    update timeout config

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ import { devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: './tests',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
@@ -41,7 +41,7 @@ const config: PlaywrightTestConfig = {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
     headless: false,
-    navigationTimeout: 30000,
+    // navigationTimeout: 30000,
   },
 
   /* Configure projects for major browsers */
--- tests/demo-codegen.spec.ts
+++ tests/demo-codegen.spec.ts
@@ -1,5 +1,22 @@
-import { test } from "@playwright/test";
+import { test, expect } from '@playwright/test';
 
-test("Demo codegen", async ({ page }) => {
-  page.goto("https://saigontechnology.com/");
+test('test', async ({ page }) => {
+
+  // Go to https://saigontechnology.com/
+  await page.goto('https://saigontechnology.com/');
+
+  // Click #sts-header a:has-text("TECHNOLOGY")
+  await page.locator('#sts-header a:has-text("TECHNOLOGY")').click();
+  await expect(page).toHaveURL('https://saigontechnology.com/technology');
+
+  // Click text=HOME COMPANY ABOUT US METHODOLOGIES CAREERS SERVICES Offshore Software Developme >> i
+  await page.locator('text=HOME COMPANY ABOUT US METHODOLOGIES CAREERS SERVICES Offshore Software Developme >> i').click();
+
+  // Fill input[name="q"]
+  await page.locator('input[name="q"]').fill('developer');
+
+  // Press Enter
+  await page.locator('input[name="q"]').press('Enter');
+  await expect(page).toHaveURL('https://saigontechnology.com/search?q=developer');
+
 });
