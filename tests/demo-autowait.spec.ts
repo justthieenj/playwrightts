3d9e83ccce650f729730d2300e6c001f3afa2e87
Thien Nguyen <ngkhanhthien94@gmail.com>
2022-10-04 14:37:20 +0700

    :hammer: update auto-waiting demo script

--- tests/demo-autowait.spec.ts
+++ tests/demo-autowait.spec.ts
@@ -1,14 +1,21 @@
-import { expect, test } from "@playwright/test";
+import { expect, Locator, test } from "@playwright/test";
 
 test("Test click", async ({ page }) => {
-  await page.goto("http://127.0.0.1:3000/index.html");
   /* 
-  login button: disabled for 3s
+  login button:
+  -> disabled for 3s
   -> then get obscured by signup button for 3s
   -> then have animation for 3s
-  -> click successful
+  -> element stable to perform click
   */
-  const loginButton = page.locator("#login");
+
+  // navigate to url
+  await page.goto("http://127.0.0.1:3000/index.html");
+
+  // click button with id = "login"
+  const loginButton: Locator = page.locator("#login");
   await loginButton.click();
+
+  // verify button has text "Logged in!"
   await expect(loginButton).toHaveText("Logged in!");
 });
